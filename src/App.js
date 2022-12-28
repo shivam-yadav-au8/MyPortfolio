19404f202022027cf5504f1c3c80bda62797db01
Shivam Yadav <shivam.yadav@rattanIndia.com>
2022-12-28 17:15:04 +0530

    feat: updated the files

--- src/App.js
+++ src/App.js
@@ -13,7 +13,26 @@ function App() {
         <div className="lb3"></div>
         <div className="lb4"></div>
         <div className="lb5"></div>
+        <div className="lb6"></div>
+        <div className="lb7"></div>
+        <div className="lb8"></div>
+        <div className="lb9"></div>
+        <div className="lb10"></div>
+        <div className="lb11"></div>
+        <div className="lb12"></div>
+        <div className="lb13"></div>
+        <div className="lb14"></div>
+        <div className="lb15"></div>
+        <div className="lb16"></div>
+        <div className="lb17"></div>
+        <div className="lb18"></div>
+        <div className="lb19"></div>
+        <div className="lb20"></div>
+        <div className="lb21"></div>
+        <div className="lb22"></div>
+        <div className="lb23"></div>
       </div>
+      
       <div className="bbar"></div>
       {/* <div className="frame">
         <img src={im}></img>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,39 +1,48 @@
-import React, { Component } from 'react';
-import Navbar from '../Navbar/Navbar';
-import Projects from '../Projects/Projects';
+import React, { Component } from "react";
+import Navbar from "../Navbar/Navbar";
+import Projects from "../Projects/Projects";
 import Experience from "../Experience/Experience";
 import CV from "../CV/CV";
-import Contact from '../Contact/Contact';
+import Contact from "../Contact/Contact";
 import Footer from "../Footer/Footer";
-import './Main.scss';
+import "./Main.scss";
+import shivam from "../../assests/shivam.png";
 class Main extends Component {
-    state = {  }
-    render() { 
-        return (<div className="expand"><div className="main">
-            <Navbar></Navbar>
-            <div className="title">
-                <h1>Hey, I'm</h1>
+  state = {};
+  render() {
+    return (
+      <div className="expand">
+        <div className="main">
+          <Navbar></Navbar>
+          <div className="title">
+            <h1>Hey, I'm</h1>
+          </div>
+          <div className="name">
+            <h1>Shivam</h1>
+            <div className="yu"></div>
+          </div>
+          <img class="my-pic" src={shivam} alt="img"></img>
+          <div className="bb">
+            <div className="text">
+              I'm a Full Stack developer with a knack of coding.I am extremely
+              driven with a clear goal to succeed and I love
+              calligraphy,sketching etc.I am commited to learning and
+              self-development so that I can consistently achieve better
+              results.{" "}
             </div>
-            <div className="name">
-                <h1 >Shivam</h1>
-                <div className="yu"></div>
-                </div>
-                <div className="bb">
-                   
-                    <div className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
-                </div>
-                <Projects/>
-                <div className="boundary"></div>
-                
-               
+          </div>
+          <Projects />
+          <div className="boundary"></div>
         </div>
         <div className="exp-con">
-        <Experience/>
-        <CV/>
-        <Contact/>
-        <Footer/>
-        </div></div>);
-    }
+          <Experience />
+          <CV />
+          <Contact />
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Main;
