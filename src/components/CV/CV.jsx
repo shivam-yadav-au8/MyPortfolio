55a256b2a62c1325f08d8e864f0e839bd32ac16c
Shivam Yadav <shivam.yadav@rattanIndia.com>
2022-12-29 17:20:42 +0530

    update resume

--- src/components/CV/CV.jsx
+++ src/components/CV/CV.jsx
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
-import './CV.scss';
+import React, { Component, Fragment } from "react";
+import ExampleDoc from "../../assests/Resume.pdf";
+import "./CV.scss";
 class CV extends Component {
-    state = {  }
-    render() { 
-        return (<div className="cv"><button className="bt">Download Resume</button></div>  );
-    }
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <div className="cv">
+          <a
+            href={ExampleDoc}
+            download="MyExampleDoc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="bt">Download Resume</button>
+          </a>
+        </div>
+      </Fragment>
+    );
+  }
 }
- 
+
 export default CV;
